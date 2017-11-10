60e6d79a4969f671d1349b79fc4717a558d956b1
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-11-10 04:19:54 -0200

    trazendo ultimo id inserido

--- index.php
+++ index.php
@@ -12,28 +12,37 @@ function get_id($conexao)
 	}
 }
 
+function get_user($id = NULL, $conexao)
+{
+	$query = 'select id_char, nome from usuario';
+
+	if($id!=NULL) {
+		$query .= ' where id = GENERATE_BINARY_UUID("'.$id.'")';
+	}else{
+		if(isset($_GET['id']) && !empty($_GET['id'])) 
+			$query .= ' where id = GENERATE_BINARY_UUID("'.$_GET['id'].'")';
+	}
+
+	$q = $conexao->query($query);
+	$usuarios = [];
+	while($item = $q->fetch_all()) {
+		$usuarios[] = $item;
+	}
+	return $usuarios;
+}
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$id = get_id($conexao);
 	$query = "insert into usuario(id, id_char, nome) value(GENERATE_BINARY_UUID('".$id."'),'".$id."', '".$_POST['nome']."')";
 	if($conexao->query($query)) {
-		$msg = 'Inserido com sucesso';
+		$msg = get_user($id, $conexao);
 	}else{
 		$msg = 'Erro na inserção';
 	}
 
 	echo json_encode($msg);
 }else{
-
-	$query = 'select id_char, nome from usuario';
-	if(isset($_GET['id']) && !empty($_GET['id'])) 
-		$query .= ' where id = GENERATE_BINARY_UUID("'.$_GET['id'].'")';
-	
-	$q = $conexao->query($query);
-	$retorno = [];
-	while($item = $q->fetch_all()) {
-		$retorno[] = $item;
-	}
-
-	echo json_encode($retorno);
+	$usuario = get_user(NULL, $conexao);
+	echo json_encode($usuario);
 }
 
